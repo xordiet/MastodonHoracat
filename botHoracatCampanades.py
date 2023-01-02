512a919d63841eb452ebc652cd1595d774edd385
Jordi Borràs i Vivó <jordibv@gmail.com>
2023-01-02 23:44:34 +0100

    2.2 (#5)
    * 2.2 campanades
    
    Redueixo la freqüència de publicació i li afegeixo campanades
    
    * 2.1 campanades
    
    corregit error en condicionals

--- botHoracatCampanades.py
+++ botHoracatCampanades.py
@@ -14,29 +14,29 @@ hora = now.hour
 minut = now.minute
 any = now.year
 
-if hora = 1 or hora = 13:
+if hora == 1 or hora == 13:
 	mastodon.status_post("Dong") #1
-elif hora = 2 or hora = 14:
+elif hora == 2 or hora == 14:
 	mastodon.status_post("Dong, Dong") #2
-elif hora = 3 or hora = 15:
+elif hora == 3 or hora == 15:
 	mastodon.status_post("Dong, Dong, Dong") #3
-elif hora = 4 or hora = 16:
+elif hora == 4 or hora == 16:
 	mastodon.status_post("Dong, Dong, Dong, Dong") #4
-elif hora = 5 or hora = 17:
+elif hora == 5 or hora == 17:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong") #5
-elif hora = 6 or hora = 18:
+elif hora == 6 or hora == 18:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong") #6
-elif hora = 7 or hora = 19:
+elif hora == 7 or hora == 19:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong, Dong") #7
-elif hora = 8 or hora = 20:
+elif hora == 8 or hora == 20:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong") #8
-elif hora = 9 or hora = 21:
+elif hora == 9 or hora == 21:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong") #9
-elif hora = 10 or hora = 22:
+elif hora == 10 or hora == 22:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong") #10
-elif hora = 11 or hora = 23:
+elif hora == 11 or hora == 23:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong") #11
-elif hora = 12 or hora = 0:
+elif hora == 12 or hora == 0:
 	mastodon.status_post("Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong, Dong") #12
 else:
 	mastodon.status_post("Error al tocar les campanades, aviseu al campaner")
